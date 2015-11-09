ebdcaf14e8101bd753a865dd6a80ad8476dae6f3
Aakash <aakashsethi20@gmail.com>
2015-11-09 15:05:34 -0500

    Not complete solution

--- Problem3.java
+++ Problem3.java
@@ -1,13 +1,46 @@
 
+/**
+ * Problem 3 Largest Prime Factor
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * @author Aakash
+ * created on 2015-11-08
+ */
+
 public class Problem3 {
 	public static void main(String[] args) {
-		
+		long n = 600851475143l;			//taking the number as long
+		prime(n);
 	}
 	
-	public static void prime() {
-		long n = 600851475143l;
-		int n2 = (int) n/2;
-		boolean[] prime = new boolean[n2];
+	// Using the Sieve of Eratosthenes to find the prime numbers
+	// so as to find the smallest prime number of n and hence the largest
+	public static void prime(long n) {
+		int n2 = (int) Math.sqrt(n);
+		boolean[] prime = new boolean[n2+1];
 		
+		//initializing the array as all true
+		for(int i = 2; i <= n2; i++)
+			prime[i] = true;
+		
+		for(int divisor = 2; divisor*divisor <= n2; divisor++) {
+			if(prime[divisor]) {
+				for(int i = 2*divisor; i <= n2; i = i + divisor)
+					prime[i] = false;
+			}
+		}
+		
+		int check = 2;
+		while(check <= n/2) {
+			if(prime[check]) {
+				if(n%check == 0) {
+					long answer = (long) n/check;
+					System.out.println("Greatest factor of " + n + " is " + answer);
+					break;
+				}
+			}
+			check++;
+		}
 	}
 }
